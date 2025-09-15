26bda976f2e3f32c887ca139bfd3c20826e78512
Tarun <tarunkumarsharma38@gmail.com>
2025-09-15 13:13:05 +0530

    Assignment - [Assignment] Applying Event Architecture for LightSwitch Event

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -7,15 +7,15 @@ public class LightSwitchView : MonoBehaviour, IInteractable
 {
     [SerializeField] private List<Light> lightsources = new List<Light>();
     private SwitchState currentState;
-    public static event Action OnLightSwitchToggled;
+    //public static event Action OnLightSwitchToggled;
 
-    private void OnEnable() => OnLightSwitchToggled += onLightSwitch;
+    private void OnEnable() => EventService.Instance.OnLightSwitchToggled.Subscribe(onLightSwitch);
 
-    private void OnDisable() => OnLightSwitchToggled -= onLightSwitch;
+    private void OnDisable() => EventService.Instance.OnLightSwitchToggled.Unsubscribe(onLightSwitch);
 
     private void Start() => currentState = SwitchState.Off;
 
-    public void Interact() => OnLightSwitchToggled?.Invoke();
+    public void Interact() => EventService.Instance.OnLightSwitchToggled.InvokeEvent();
 
     private void toggleLights()
     {
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -25,13 +25,13 @@ public class PlayerController
 
         this.playerScriptableObject = playerScriptableObject;
         this.playerScriptableObject.KeysEquipped = 0;
-        LightSwitchView.OnLightSwitchToggled += onLightSwitch;
+         EventService.Instance.OnLightSwitchToggled.Subscribe(onLightSwitch);
         playerState = PlayerState.InDark;
     }
 
     ~PlayerController()
     {
-        LightSwitchView.OnLightSwitchToggled -= onLightSwitch;
+        EventService.Instance.OnLightSwitchToggled.Unsubscribe(onLightSwitch);
     }
     public void Interact() => IsInteracted = Input.GetKeyDown(KeyCode.E) ? true : (Input.GetKeyUp(KeyCode.E) ? false : IsInteracted);
 
