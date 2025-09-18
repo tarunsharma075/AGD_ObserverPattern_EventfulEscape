7a342124098f726a9a9522a3c5d68df717bfbc80
Tarun <tarunkumarsharma38@gmail.com>
2025-09-18 08:16:07 +0530

    Assignment - [Assignment] Coding EventTrigger of LightsTurnOffByGhostEvent

--- Assets/Scripts/Events/LightsOffByGhostEvent.cs
+++ Assets/Scripts/Events/LightsOffByGhostEvent.cs
@@ -3,4 +3,19 @@ using UnityEngine;
 public class LightsOffByGhostEvent : MonoBehaviour
 {
 
+    [SerializeField]
+    private int KeyTotrigger;
+    [SerializeField]
+    private SoundType sound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerView>()!=null&& KeyTotrigger== GameService.Instance.GetPlayerController().KeysEquipped)
+        {
+            EventService.Instance.OnLightsOffByGhostEvent.InvokeEvent();
+            GameService.Instance.GetSoundView().PlaySoundEffects(sound);
+            this.enabled = false;
+        }
+    }
+
 }
