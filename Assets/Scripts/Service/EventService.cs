23e575840b5d3ccddeca6807533f06020dd1fe92
Tarun <tarunkumarsharma38@gmail.com>
2025-09-18 07:10:10 +0530

    Assignment - [Assignment] Implementation of Generic Event Controller

--- Assets/Scripts/Service/EventService.cs
+++ Assets/Scripts/Service/EventService.cs
@@ -4,14 +4,23 @@ public class EventService
     private static EventService instance;
     public static EventService Instance
     {
+<<<<<<< Updated upstream
         get
         {
             if (instance == null)
+=======
+            if(instance == null)
+>>>>>>> Stashed changes
             {
                 instance = new EventService();
             }
             return instance;
+<<<<<<< Updated upstream
         }
+=======
+
+        } 
+>>>>>>> Stashed changes
     }
 
     public EventController OnLightSwitchToggled { get; private set; }
